7f1b78efdf5338e97fac6b300c5b10673e65df41
sin <aidenrivera7@gmail.com>
2024-09-04 11:57:59 -0400

    Formatting Changes

--- src/SlashCommandListener.h
+++ src/SlashCommandListener.h
@@ -2,10 +2,10 @@
 #include <dpp/dpp.h>
 
 class SlashCommandListener {
-public:
-  static void on_slashcommmand(const dpp::slashcommand_t &event);
+   public:
+    static void on_slashcommmand ( const dpp::slashcommand_t &event );
 
-private:
-  enum cmd { cmdWhoAmI, cmdPing, cmdConnect, cmdDisconnect, cmdNotValid };
-  static cmd cmdHash(const std::string &str);
+   private:
+    enum cmd { cmdWhoAmI, cmdPing, cmdConnect, cmdDisconnect, cmdNotValid };
+    static cmd cmdHash ( const std::string &str );
 };
--- src/main.cpp
+++ src/main.cpp
@@ -1,42 +1,49 @@
-#include "Listeners.h"
 #include <dpp/dpp.h>
+
 #include <dpp/nlohmann/json.hpp>
+
+#include "Listeners.h"
 
 static const long YDS_GUILD_ID = 920188312591933460;
 
 using json = nlohmann::json;
 
-int main(int argc, char const *argv[]) {
-  json configDocument;
-  std::ifstream configFile("../config.json");
-  configFile >> configDocument;
-  dpp::cluster bot(configDocument["token"], dpp::i_default_intents |
-                                                dpp::i_guild_members |
-                                                dpp::i_message_content);
+int main ( int argc, char const *argv[] ) {
+    json configDocument;
+    std::ifstream configFile ( "../config.json" );
+    configFile >> configDocument;
+    dpp::cluster bot ( configDocument[ "token" ],
+                       dpp::i_default_intents | dpp::i_guild_members
+                           | dpp::i_message_content );
 
-  bot.on_log(dpp::utility::cout_logger());
+    bot.on_log ( dpp::utility::cout_logger ( ) );
 
-  bot.on_slashcommand(&SlashCommandListener::on_slashcommmand);
+    bot.on_slashcommand ( &SlashCommandListener::on_slashcommmand );
 
-  bot.on_ready([&bot](const dpp::ready_t &event) -> void {
-    if (dpp::run_once<struct clear_bot_commands>()) {
-      // bot.global_bulk_command_delete();
-      bot.guild_bulk_command_delete(YDS_GUILD_ID);
-    }
+    bot.on_ready ( [ &bot ] ( const dpp::ready_t &event ) -> void {
+        if ( dpp::run_once< struct clear_bot_commands > ( ) ) {
+            // bot.global_bulk_command_delete();
+            bot.guild_bulk_command_delete ( YDS_GUILD_ID );
+        }
 
-    if (dpp::run_once<struct register_bot_commands>()) {
-      dpp::slashcommand whoami("whoami", "Information about me.", bot.me.id);
-      dpp::slashcommand ping("ping", "Wanna play?", bot.me.id);
-      dpp::slashcommand connect("connect", "Make me join vc.", bot.me.id);
-      dpp::slashcommand disconnect("disconnect", "Make me leave vc.",
-                                   bot.me.id);
+        if ( dpp::run_once< struct register_bot_commands > ( ) ) {
+            dpp::slashcommand whoami ( "whoami",
+                                       "Information about me.",
+                                       bot.me.id );
+            dpp::slashcommand ping ( "ping", "Wanna play?", bot.me.id );
+            dpp::slashcommand connect ( "connect",
+                                        "Make me join vc.",
+                                        bot.me.id );
+            dpp::slashcommand disconnect ( "disconnect",
+                                           "Make me leave vc.",
+                                           bot.me.id );
 
-      const std::vector<dpp::slashcommand> commands = {whoami, ping, connect,
-                                                       disconnect};
-      bot.guild_bulk_command_create(commands, YDS_GUILD_ID);
-    }
-  });
+            const std::vector< dpp::slashcommand > commands
+                = { whoami, ping, connect, disconnect };
+            bot.guild_bulk_command_create ( commands, YDS_GUILD_ID );
+        }
+    } );
 
-  bot.start(dpp::st_wait);
-  return 0;
+    bot.start ( dpp::st_wait );
+    return 0;
 }
